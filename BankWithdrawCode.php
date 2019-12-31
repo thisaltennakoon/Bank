bb78f0f721c9b8706c4e708801bec9e03efd2acd
GayanJayakody <gayanjayakody13@gmail.com>
2020-01-01 00:50:49 +0530

    Update BankWithdrawCode.php

--- BankWithdrawCode.php
+++ BankWithdrawCode.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['User'])& empty($_SESSION['User'])) {
+    header('location: Login.php');
+}
+?>
+
 <?php   
 $servername = "localhost";
 $username = "root";
@@ -12,7 +19,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 if(!empty($_POST["AccNo"])) {
-	echo "<br> Account Number - ".$_POST["AccNo"];
+
   $sql = "SELECT Number_of_Withdrawals,Account_Plan_ID,Balance FROM savings_account NATURAL JOIN account WHERE Account_No='". $_POST["AccNo"] ."'";
   $result = $conn->query($sql);
   if ($result->num_rows > 0){
@@ -28,25 +35,23 @@ if(!empty($_POST["AccNo"])) {
 		  }elseif ( $remainder <500 and $row["Account_Plan_ID"]==2){			  
 			  echo "Account Balance is insufficient";
 		  }else{
-			  $sql = "UPDATE account SET Balance = Balance-'" . $_POST["amount"] . "'WHERE Account_No='".$_POST["AccNo"] ."';
+			  $sql = "SET autocommit = OFF;
+			    START TRANSACTION;
+			    UPDATE account SET Balance = Balance-'" . $_POST["amount"] . "'WHERE Account_No='".$_POST["AccNo"] ."';
 				INSERT INTO transaction_detail(Account_No,Amount,Withdraw) values ('" . $_POST["AccNo"] . "','" . $_POST["amount"] . "',True);
 				INSERT INTO bank_transaction (Transaction_ID) values ((SELECT LAST_INSERT_ID()));
-				UPDATE savings_account SET Number_of_Withdrawals = Number_of_Withdrawals+1 WHERE Account_No='".$_POST["AccNo"] ."'";
+				UPDATE savings_account SET Number_of_Withdrawals = Number_of_Withdrawals+1 WHERE Account_No='".$_POST["AccNo"] ."';
+				COMMIT;";
+				echo "Withdrwal Successful";
 				$conn->multi_query($sql);
- 
-				$sql = "SELECT Balance FROM account WHERE Account_No='" . $_POST["AccNo"] . "'";
-				$results = $conn->query($sql);
+
 				unset($_POST);
 				$_POST=array();
-				if ($results->num_rows > 0){
-					while($rows = $results->fetch_assoc()) {
-						echo "<br>Balance = " .$rows["Balance"]. "";
-					}
-				}						  
+						  
 		  }	 
 	}
   }
 }
 $conn->close();	
-header( "refresh:6;url=BankTransaction.php" );
+header( "refresh:2;url=BankTransaction.php" );
 ?>
