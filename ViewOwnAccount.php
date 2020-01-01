cb3bd0956abe892e373a70d084dc124065314929
GayanJayakody <gayanjayakody13@gmail.com>
2020-01-01 22:20:33 +0530

    Update ViewOwnAccount.php

--- ViewOwnAccount.php
+++ ViewOwnAccount.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['User'])& empty($_SESSION['User'])) {
 $customer_ID = $_SESSION['Customer_ID'];
 
 ?>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  enctype="multipart/form-data">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
 	<label for="Account_No">Choose the account</label><br>
 
@@ -29,8 +29,8 @@ $customer_ID = $_SESSION['Customer_ID'];
 
 		$sql = "SELECT Account_No from account where Primary_Customer_ID=$customer_ID";
 		$results = $conn->query($sql);
-		if ($results->num_rows > 0){
-		echo'<select name="Account_No" style="width: 400px; height: 40px">';
+		if ($results->num_rows > 0){ 
+		echo '<select name="Account_No" style="width: 400px; height: 40px">';
 		
 		while($rows = $results->fetch_assoc()){       //while loop
 			echo '<option value="'.$rows['Account_No'].'">'.$rows['Account_No'].'</option>
@@ -44,7 +44,13 @@ $customer_ID = $_SESSION['Customer_ID'];
 		?>
 		<br>
 		<br>
+        <button type="submit">View Account</button>
+</form>
 <?php 
+
+if(isset($_POST) & !empty($_POST)){
+    $Account_No = $_POST['Account_No'];
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -56,30 +62,28 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-$Account_No=$_POST['Account_No'];
-$query = "SELECT * FROM transaction_detail WHERE Account_No='$Account_No'";
+
+$query = "SELECT * FROM transaction_detail WHERE Account_No='$Account_No' ORDER BY Transaction_ID";
  
  
 echo '<table border="0" cellspacing="2" cellpadding="2"> 
       <tr> 
-          <td> <font face="Arial">Transaction ID</font> </td> 
-          <td> <font face="Arial">Account Number</font> </td> 
+          <td> <font face="Arial">Date and Time</font> </td>  
           <td> <font face="Arial">Amount</font> </td> 
           <td> <font face="Arial">Withdraw</font> </td> 
-          <td> <font face="Arial">Detail</font> </td>
-          <td> <font face="Arial">Date and Time</font> </td> 
+          <td> <font face="Arial">Balance</font> </td>
+          <td> <font face="Arial">Detail</font> </td> 
           <td> <font face="Arial">Teller</font> </td>
       </tr>';
  
 if ($result = $conn->query($query)) {
     while ($row = $result->fetch_assoc()) {
-        $field1name = $row["Transaction_ID"];
-        $field2name = $row["Account_No"];
-        $field3name = $row["Amount"];
-        $field4name = $row["Withdraw"];
+        $field1name = $row["Date_Time"];
+        $field2name = $row["Amount"];
+        $field3name = $row["Withdraw"];
+        $field4name = $row["Balance"];
         $field5name = $row["Detail"];
-        $field6name = $row["Date_Time"];
-        $field7name = $row["Teller"]; 
+        $field6name = $row["Teller"]; 
  
         echo '<tr> 
                   <td>'.$field1name.'</td> 
@@ -88,11 +92,11 @@ if ($result = $conn->query($query)) {
                   <td>'.$field4name.'</td> 
                   <td>'.$field5name.'</td> 
                   <td>'.$field6name.'</td>
-                  <td>'.$field7name.'</td>
               </tr>';
     }
     $result->free();
 } 
+}
 ?>
 </body>
 </html>
