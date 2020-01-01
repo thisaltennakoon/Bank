9516d03c34ad52809c0f1e42e8f189129e58b76f
GayanJayakody <gayanjayakody13@gmail.com>
2020-01-02 04:36:32 +0530

    Revert "Update ApproveLoans1.php"
    This reverts commit 45bb761a621ba0237525d758bf9fbd8e8011c0b9.

--- ApproveLoans1.php
+++ ApproveLoans1.php
@@ -41,7 +41,7 @@ if(!isset($_SESSION["User"]) & empty($_SESSION["User"])){
         $query = "INSERT INTO loan(Account_No,Loan_Type,Amount,Branch_ID,Time_Period,Installment) VALUES($AccNo,1,$Amount,$Branch,$Time_Period,$Installment)";
         $result = $conn->query($query);
         if ( !$result ) {
-            //$result->free();
+            $result->free();
             throw new Exception($conn->error);
         }
     
@@ -50,39 +50,32 @@ if(!isset($_SESSION["User"]) & empty($_SESSION["User"])){
         $query = "INSERT INTO bank_visit_loan(Loan_ID,Approved_By,Requested_BY) VALUES($Loan_ID,$User,$RequestBy) ";
         $result = $conn->query($query);
         if ( !$result ) {
-            //$result->free();
+            $result->free();
             throw new Exception($conn->error);
         }
 
         
-        $query = "INSERT INTO transaction_detail(Account_No,Amount,Withdraw,Balance,Detail,Teller) VALUES($AccNo,$Amount,False,1000,'Loan Granted','self')";
+        $query = "INSERT INTO transaction_detail(Account_No,Amount,Withdraw) VALUES($AccNo,$Amount,False)";
         $result = $conn->query($query);
         if ( !$result ) {
-           // $result->free();
+            $result->free();
             throw new Exception($conn->error);
         }
 
         $query = "UPDATE requested_loan SET Request_Status='Approved' WHERE Request_ID=$RequestID";
         $result = $conn->query($query);
         if ( !$result ) {
-            //$result->free();
+            $result->free();
             throw new Exception($conn->error);
         }
 
         $query = "UPDATE account SET Balance=Balance+".$Amount." WHERE Account_No=$AccNo";
         $result = $conn->query($query);
         if ( !$result ) {
-            //$result->free();
-            throw new Exception($conn->error);
-        }
-
-        $query = "CREATE EVENT settleInstallment5 ON SCHEDULE EVERY 1 MINUTE STARTS '2020-01-02 00:00:00' DO INSERT INTO transaction_details(Account_No,Amount,Withdraw,Balance,Detail,Teller) VALUES($AccNo,$Amount,True,1000,'Installment','Self');
-                    SET GLOBAL event_scheduler='ON';";
-        $result = $conn->multi_query($query);
-        if ( !$result ) {
             $result->free();
             throw new Exception($conn->error);
         }
+    
         // our SQL queries have been successful. commit them
         // and go back to non-transaction mode.
     
