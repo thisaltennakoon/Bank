2fcc54938462cb56360305213060787d046dbacb
Nuwanga Herath <nuwangamihiruwan@gmail.com>
2020-01-01 23:11:56 +0530

    tetsfyt

--- BankTransaction.php
+++ BankTransaction.php
@@ -1,3 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple login form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <style>
+	.header {
+  	background-color: #ff4500;
+
+	width: 100%;
+	
+    background-size: 100%;
+	background-repeat: no-repeat;
+    background-size: cover;
+    margin-left: auto;
+    margin-right: auto;
+  	padding: 20px;
+  	text-align: center;
+	position: absolute;
+  	left: 0px;
+  	top: 0px;
+	}
+      html {
+
+      display: flex;
+      justify-content: center;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 15px;
+
+      } 
+	  body {
+      display: flex;
+      justify-content: center;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 15px;
+	  background-color: #FF8C00;
+	  
+      }
+      form {
+      border: 6px solid #FF7F50;
+	  padding: 25px 50px;
+	  position: absolute;
+  	  top: 120px;
+	  background-color: #FF7F50;
+
+      }
+      input[type=text], input[type=password] {
+      width: 100%;
+      padding: 16px 8px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      }
+      button {
+      background-color: #006400;
+
+      color: white;
+      padding: 14px 0;
+      margin: 10px 0;
+      border: none;
+      cursor: grabbing;
+      width: 100%;
+      }
+      h1 {
+      text-align:center;
+      fone-size:18;
+      }
+      button:hover {
+
+        background-color: #00FF00;
+
+      }
+      .formcontainer {
+      text-align: left;
+      margin: 24px 50px 12px;
+	  
+      }
+      .container {
+      padding: 16px 0;
+      text-align:left;
+      }
+      span.psw {
+      float: right;
+      padding-bottom: 0;
+      padding-right: 15px;
+      }
+	  span.aaa {
+      float: left;
+      padding-bottom: 0;
+      padding-left: 15px;
+      }
+      /* Change styles for span on extra small screens */
+      @media screen and (max-width: 300px) {
+      span.psw {
+      display: block;
+      float: none;
+      }
+    </style>
+  </head>
+
+  <div class="header">
+  <h1>BANK A SEYCHELLES</h1>
+</div>
+<body>
 <?php
 session_start();
 if (!isset($_SESSION['User'])& empty($_SESSION['User'])) {
@@ -23,14 +128,20 @@ function checkAvailability() {
 </script>
 
 <form method="post" action ="BankWithdrawCode.php" enctype="multipart/form-data">
-
-    <label for="AccNo">Enter Account Number</label>
+	<h1>Transactions</h1>
+	<div class="formcontainer">
+      <hr/>
+  <div class="container"></div>
+    <label for="AccNo"><strong>Enter Account Number:</strong></label>
         <input type="number" id="AccNo" name="AccNo" placeholder="" onBlur="checkAvailability()"><span id="user-availability-status"></span>	
         <br>
 		<br>
-	<label for="amount">Enter the amount</label>
+	<label for="amount"><strong>Enter the amount    :</strong></label>
         <input type="number" step="0.01" id="amount" name="amount" placeholder="0">		
 		<br>
+		</div>
 		<button type="submit" formaction="BankWithdrawCode.php">Withdraw</button>
 		<button type="submit" formaction="BankDepositCode.php">Deposit</button>
 </form>
+</body>
+</html>
