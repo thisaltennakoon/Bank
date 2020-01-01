d45adb5bc07d09077380222542c063f64df37702
GayanJayakody <gayanjayakody13@gmail.com>
2020-01-01 20:11:10 +0530

    Update BankDepositCode.php

--- BankDepositCode.php
+++ BankDepositCode.php
@@ -23,7 +23,7 @@ if(!empty($_POST["AccNo"])) {
   $sql = "SET autocommit = OFF;
   START TRANSACTION;
   UPDATE account SET Balance = Balance+'" . $_POST["amount"] . "'WHERE Account_No='".$_POST["AccNo"] ."';
-  INSERT INTO transaction_detail(Account_No,Amount,Withdraw) values ('" . $_POST["AccNo"] . "','" . $_POST["amount"] . "',False);
+  INSERT INTO transaction_detail(Account_No,Amount,Withdraw,Detail,Teller) values ('" . $_POST["AccNo"] . "','" . $_POST["amount"] . "',False,'BNK','".$_SESSION['User']."');
   INSERT INTO bank_transaction (Transaction_ID) values ((SELECT LAST_INSERT_ID())); 
   COMMIT;";
   echo "Deposit Successful";
